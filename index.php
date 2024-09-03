79050fd9b77b4d48f793f30a5268a38b4e538e3c
Carminelm <carminelm54@gmail.com>
2024-09-03 17:28:12 +0200

    add echo

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ require_once __DIR__ . '/data/db.php';
 <body>
 <?php foreach($movies as $movie): ?>
             <div>
-                 <h1>Titolo : <?php $movie->printName(); ?></h1>
+                 <h1>Titolo : <?php echo $movie->title; ?></h1>
                  <h2>>Anno : <?php $movie->printData(); ?></h2>
                              
             </div>
