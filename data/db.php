9720ba32055ff7dfa2e2f89a93743c74e0c49d49
Carminelm <carminelm54@gmail.com>
2024-09-03 16:08:09 +0200

    add db

---
+++ data/db.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once __DIR__ . '/../Movie/movie.php';
+
+$movies[
+    new Movie  ('il signore degli anelli','fantasy',2001),
+    new Movie  ('il corvo','horoor',1994),
+];
+
+
+?>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__ . '/Movie/movie.php';
+require_once __DIR__ . '/data/db.php';
+
+
+
+
+
+?>
