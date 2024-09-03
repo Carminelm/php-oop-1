883cb16404df9efc4bbe4d6dd421653d591b4260
Carminelm <carminelm54@gmail.com>
2024-09-03 15:33:48 +0200

    add new function

---
+++ Movie/movie.php
@@ -0,0 +1,14 @@
+<?php
+
+class Movie {
+    public $title;
+    public $type;
+    public $date;
+
+
+    function __construct ($_title,$_type,$_date){
+        $this->$title;
+        $this->$type;
+        $this->$date;
+    }
+}
